fdf373a174f6d3e20f6bbfdf94aecee989479650
Rafael Mata <rafaelmata357@gmail.com>
2020-06-24 20:02:41 -0600

    Refactor: Graph function definition

--- logistic_map_function.py
+++ logistic_map_function.py
@@ -8,9 +8,10 @@ maxNum : Cantidad max de generaciones, en tiempo
 import matplotlib.pyplot as plt
 
 import numpy as np
+import matplotlib.ticker as ticker
 
 
-def f(lamb,x,maxNum): 
+def log_function(lamb,x,maxNum): 
     result = [] 
 
     for i in range(1,maxNum+1): 
@@ -20,17 +21,36 @@ def f(lamb,x,maxNum):
   
     return result 
 
-x = np.linspace(0,3.9,400) 
-y = [f(l,0.5,200) for l in x] 
-x2 = [lamb[-150:] for lamb in y]
-z = [[i]*150 for i in x] 
-plt.figure(figsize=(10,6))
-plt.scatter(z[:400],x2[:400],s=0.5)
+def graph(x,y):
+    ''' Function to graph the log_function
+    Params:
+          x: list with the lambda values for each iteration
+          y: the log_function output
 
-plt.xlim(0,4)
-plt.grid(True,ls=':',color='k')
-plt.title('Logistic Map Function: ' + r'$X_{n+1}  =  \lambda   X_n (1-X_n)$', fontsize=20)
+    returns:
+           None
+    '''
+
+    plt.figure(figsize=(10,6))
+    plt.scatter(x[:400], y[:400],s=0.1)
+    tick_major = 0.2
+    tick_minor = 0.1
+    plt.xlim(0,4.1)
+    plt.grid(True,ls=':',color='k')
+    plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(tick_major)) 
+    plt.gca().xaxis.set_minor_locator(ticker.MultipleLocator(tick_minor))
+    plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(0.1)) 
+    plt.gca().yaxis.set_minor_locator(ticker.MultipleLocator(0.05))
+    #plt.xticks(np.arange(0,4.2,0.2))
+    plt.title('Logistic Map Function: ' + r'$X_{n+1}  =  \lambda   X_n (1-X_n)$', fontsize=18,c='b')
+    plt.xlabel(r'$\lambda$',fontsize=30,c='b')
+    plt.show()
 
 
-plt.xlabel(r'$\lambda$',fontsize=30)
-plt.show()
+if __name__ == '__main__':  
+
+    x = np.linspace(0,4,400) 
+    y = [log_function(l,0.5,200) for l in x] 
+    x2 = [lamb[-150:] for lamb in y]
+    z = [[i]*150 for i in x] 
+    graph(z, x2)

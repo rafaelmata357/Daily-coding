cacfe439f7c13ee4024ca6bdfd39615c33dde80f
Rafael Mata <rafaelmata357@gmail.com>
2023-02-14 14:27:17 -0600

    FEAT: Add new fuction to calculate fibonacci series

--- oop_sum.py
+++ oop_sum.py
@@ -1,15 +1,39 @@
 class Calculator:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+    ''' Class to create different aritmentic functions
+    '''
+    
+    def __init__(self):
+        #self.num1 = num1
+        #self.num2 = num2
+        return None
 
-    def add(self):
-        return self.num1 + self.num2
+    def add(self,x,y):
+        return x+y
 
-# Create a Calculator object with two numbers
-calc = Calculator(5, 10)
+    def fib(self, n):
+        ''' Fucntion to calculate the n-element in the Fibonacci series
 
-# Call the add method on the Calculator object to get the sum of the two numbers
-sum = calc.add()
+            Params: n integer, n-element
 
-print("The sum is:", sum)
+            Returns: fib_series , list
+        '''
+
+    
+
+
+
+
+if __name__ == '__main__': 
+
+
+    # Create a Calculator object with two numbers
+
+
+
+
+     calc = Calculator()
+
+     # Call the add method on the Calculator object to get the sum of the two numbers
+     sum = calc.add(10,2)
+
+     print("The sum is:", sum)
